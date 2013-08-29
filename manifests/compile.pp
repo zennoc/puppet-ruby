fd56224147de840d5597182952af7bc01f4dd7a9
Alessandro Franceschi <al@lab42.it>
2013-08-29 12:30:23 +0200

    Lints

--- manifests/compile.pp
+++ manifests/compile.pp
@@ -17,9 +17,9 @@ class ruby::compile {
   $short_version="${short_versions[0]}.${short_versions[1]}"
   puppi::netinstall { 'ruby_source':
     url                 => "http://ftp.ruby-lang.org/pub/ruby/${short_version}/ruby-${ruby::version}.zip",
-    destination_dir     => "/var/tmp",
+    destination_dir     => '/var/tmp',
     extracted_dir       => "ruby-${ruby::version}",
-    postextract_command => "sh configure && make && make test && make install",
+    postextract_command => 'sh configure && make && make test && make install',
     require             => Puppi::Install_packages['ruby_compile_prerequisites'],
   }
 
